f1f1ecb7e20bbd7800844357f1f7180cd192d405
Cameron Ray <27529775+upsmancsr@users.noreply.github.com>
2019-10-24 22:36:18 -0700

    Created endpoint to get single team; tested with Postman.

--- backend/routes/teams.js
+++ backend/routes/teams.js
@@ -14,8 +14,25 @@ router.get('/', async (req, res) => {
         res.status(200).json(teams);
     }
     catch(error) {
+        console.log(error);
         res.status(500).json(error);
     }
 });
+
+router.get('/singleteam/:teamId', async (req, res) => {
+    try {
+        const { uid } = req.body;
+        const { teamId } = req.params;
+
+        const user = await User.findOne({ uid });
+        const team = await Team.find({ _id: teamId }, { users: user._id}).select('name users adminUsers');
+        console.log('team:', team);
+        res.status(200).json(team);
+    }
+    catch(error) {
+        console.log(error);
+        res.status(500).json(error);
+    }
+})
 
 module.exports = router;
