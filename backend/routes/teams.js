33f12002a3ab8d58d35f436f7ede224a80effe86
Cameron Ray <27529775+upsmancsr@users.noreply.github.com>
2019-10-24 21:52:15 -0700

    Created teams.js file in routes folder and imported routes to index.js

---
+++ backend/routes/teams.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+// Import MongoDB models:
+const User = require('../schemas/User');
+const Team = require('../schemas/Team');
+
+
+
+module.exports = router;
