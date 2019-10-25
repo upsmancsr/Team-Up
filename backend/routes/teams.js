27917f7506ee4da8b535e4a15269fd7125734e97
Cameron Ray <27529775+upsmancsr@users.noreply.github.com>
2019-10-24 22:06:16 -0700

    Created get endpoint /teams to get a user's teams. Need to test with frontend.

--- backend/routes/teams.js
+++ backend/routes/teams.js
@@ -4,6 +4,18 @@ const router = express.Router();
 const User = require('../schemas/User');
 const Team = require('../schemas/Team');
 
-
+router.get('/', async (req, res) => {
+    try {
+        const { uid } = req.body;
+        const user = await User.findOne({ uid });
+        console.log('user:', user);
+        const teams = await Team.find({ users: user._id });
+        console.log('teams:', teams);
+        res.status(200).json(teams);
+    }
+    catch(error) {
+        res.status(500).json(error);
+    }
+});
 
 module.exports = router;
