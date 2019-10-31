05c1ff86666d70171696a18fe4492927cd2c3ebc
Cameron Ray <27529775+upsmancsr@users.noreply.github.com>
2019-10-30 17:01:43 -0700

    Created initial boilerplate for user reducer, rootReducer and store.

---
+++ frontend/src/Redux/reducers/user.js
@@ -0,0 +1,63 @@
+import axios from 'axios';
+
+// const initialState = {
+//   id: null,
+//   username: '',
+//   email: '',
+//   wins: 0,
+//   losses: 0,
+//   token: localStorage.getItem('token')
+// };
+
+// const SET_TOKEN = 'SET_TOKEN';
+// const SET_ACCOUNT_INFO = 'SET_ACCOUNT_INFO';
+
+// export default (state = initialState, action) => {
+//   switch (action.type) {
+//     case SET_TOKEN:
+//       return { ...state, token: action.payload };
+//     case SET_ACCOUNT_INFO:
+//       return { ...state, ...action.payload };
+//     default:
+//       return state;
+//   }
+// };
+
+// export const authAccount = form => async dispatch => {
+//   const accountInfo = await axios.post('/auth', form);
+//   if (accountInfo) {
+//     dispatch({ type: SET_ACCOUNT_INFO, payload: accountInfo.data });
+//   }
+// };
+
+// export const setToken = payload => {
+//   payload
+//     ? localStorage.setItem('token', payload)
+//     : localStorage.removeItem('token');
+//   return { type: SET_TOKEN, payload };
+// };
+
+const initialState = {
+    id: null,       // user._id in MongoDB 
+    firstName: '',  
+    lastName: '',
+    email: '',
+};
+  
+const SET_USER_INFO = 'SET_USER_INFO';
+
+export default (state = initialState, action) => {
+    switch (action.type) {
+        case SET_USER_INFO:
+        return { ...state, ...action.payload };
+        default:
+        return state;
+    }
+};
+
+export const authAccount = form => async dispatch => {
+    const accountInfo = await axios.post('/auth', form);
+    if (accountInfo) {
+        dispatch({ type: SET_USER_INFO, payload: accountInfo.data });
+    }
+};
