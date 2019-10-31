27f2b8404c381ab576ab25aafdd04956862a5ad3
Cameron Ray <27529775+upsmancsr@users.noreply.github.com>
2019-10-30 17:39:11 -0700

    Modified user reducer SET_USER_INFO to store user info fields in Redux state.

--- frontend/src/Redux/reducers/user.js
+++ frontend/src/Redux/reducers/user.js
@@ -49,15 +49,20 @@ const SET_USER_INFO = 'SET_USER_INFO';
 export default (state = initialState, action) => {
     switch (action.type) {
         case SET_USER_INFO:
-        return { ...state, ...action.payload };
+            return { 
+                ...state, 
+                id: action.payload._id,
+                firstName: action.payload.firstName,
+                lastName: action.payload.lastName,
+                email: action.payload.email 
+            };
         default:
-        return state;
+            return state;
     }
 };
 
 export const setUserInfo = idToken => async dispatch => {
-    const userInfo = await axios.get('/users/currentuser');
-    console.log(userInfo.data);
+    const userInfo = await axios.get('api/users/currentuser');
     dispatch({ 
         type: SET_USER_INFO, 
         payload: userInfo.data 
