3e478c2dc7fb182baf9773d72925fb92f92ccb05
Cameron Ray <27529775+upsmancsr@users.noreply.github.com>
2019-11-04 23:45:00 -0800

    Added populate taggedUsers to notes get endpoint. Modifed note card item to displays first and last name of each tagged user.

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -9,9 +9,7 @@ const Note = require('../schemas/Note');
 router.get('/:teamId', async (req, res) => {
     try {
         const { teamId } = req.params;
-        const notes = await Note.find({ team: teamId }).populate('author');
-        // console.log('user:', user);
-        // console.log('teams:', teams);
+        const notes = await Note.find({ team: teamId }).populate('author taggedUsers');
         res.status(200).json(notes);
     }
     catch(error) {
--- frontend/src/components/TeamNotes.js
+++ frontend/src/components/TeamNotes.js
@@ -125,6 +125,7 @@ function TeamNotes(props) {
                             <div className={styles.noteCard} key={index}>
                                 <p>Posted by: <b>{note.author.firstName} {note.author.lastName}</b></p>
                                 <p>Title: <b>{note.title}</b></p>
+                                <p>Tagged: {note.taggedUsers.map((user, index) => <span key={index}>{'@'}{user.firstName}{' '}{user.lastName}{' '}</span>)}</p>
                                 <p>{note.content}</p>
                             </div>
                         )
