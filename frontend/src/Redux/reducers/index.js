92f457905cd40185ffbd88dfa9d487ec8e2e937b
Cameron Ray <27529775+upsmancsr@users.noreply.github.com>
2019-11-08 16:15:29 -0800

    Added teamNotes to rootReducer

--- frontend/src/Redux/reducers/index.js
+++ frontend/src/Redux/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
 import user from './user.js';
+import teamNotes from './teamNotes.js';
 
 export default combineReducers({
-  user
+  user,
+  teamNotes
 });
